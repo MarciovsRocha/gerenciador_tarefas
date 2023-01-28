c3ab37ab9b60f06c82ff652650a421bb1acf9f81
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-01-28 12:18:58 -0300

    criado component para botão adicionar

---
+++ src/app/botoes/btn-adicionar/btn-adicionar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BtnAdicionarComponent } from './btn-adicionar.component';
+
+describe('BtnAdicionarComponent', () => {
+  let component: BtnAdicionarComponent;
+  let fixture: ComponentFixture<BtnAdicionarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BtnAdicionarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BtnAdicionarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/botoes/btn-adicionar/btn-adicionar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-adicionar',
+  templateUrl: './btn-adicionar.component.html',
+  styleUrls: ['./btn-adicionar.component.css']
+})
+export class BtnAdicionarComponent {
+
+}
