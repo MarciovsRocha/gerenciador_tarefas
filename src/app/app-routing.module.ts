c1240e90bb683caee6447d94b156607f5a89a43a
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-01-28 11:41:20 -0300

    configuração de rotas para Listar Tarefas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TarefaRoutes } from "./tarefas";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // para a URL padrão deve-se exibir a lista de tarefas
+  {
+    path: '',
+    redirectTo: 'tarefas/listar',
+    pathMatch: "full"
+  },
+  // concatena o array de rotas de TarefaRoutes ao array que será criado 
+  ...TarefaRoutes 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/tarefas/tarefas.routing.module.ts
@@ -0,0 +1,21 @@
+import { Routes } from "@angular/router";
+import { ListarTarefaComponent } from "./listar";
+
+export const TarefaRoutes: Routes = [
+    {
+        path: 'tarefas'
+        , redirectTo: 'tarefas/listar'
+    },
+    {
+        path: "tarefas/listar",
+        component: ListarTarefaComponent
+    } /*,
+    {
+        path: "tarefas/cadastrar",
+        component: CadastrarTarefaComponent
+    },
+    {
+        path: "tarefas/editar/:id",
+        component: EditarTarefaComponent
+    }*/
+];
