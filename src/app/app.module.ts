c0a409b92acbf5ce51aed59bd3df6c8c3905ea7d
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-01-29 22:13:20 -0300

    criado componente para editar tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TarefasModule } from './tarefas';
+import { EditarTarefasComponent } from './editar/editar-tarefas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditarTarefasComponent
   ],
   imports: [
     BrowserModule,
